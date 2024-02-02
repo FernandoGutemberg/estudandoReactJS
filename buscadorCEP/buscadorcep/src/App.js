9a6aa5a28deb85a84b1aec59fb6cf0d592fb0c86
FernandoGutemberg <fernando70sil@gmail.com>
2024-02-01 23:06:12 -0300

    Criando e editando o styles.css
    Altas funcionalidades.

---
+++ buscadorCEP/buscadorcep/src/App.js
@@ -0,0 +1,35 @@
+import { FiSearch } from 'react-icons/fi';
+import './styles.css';
+function App() {
+  return (
+    <div className="container">
+      <h1 className="title">Buscador CEP</h1>
+
+      <div className="containerInput">
+        <input 
+        type="text"
+        placeholder="Digite seu cep..."
+        
+        />
+
+        <button className="buttonSearch">
+          <FiSearch size={25} color="#FFF"/>
+        </button>
+
+      </div>
+
+      <main className='main'>
+        <h2>CEP: 79003222</h2>
+
+        <span>Rua Teste algum</span>
+        <span>Complemento: Algum Complemento</span>
+        <span>Vila Rosa</span>
+        <span>Campo Grande - MS</span>
+
+      </main>
+
+    </div>
+  );
+}
+
+export default App;
