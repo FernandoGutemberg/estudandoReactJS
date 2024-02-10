e90430f3980f044fec72d584ab429df8c534de3d
FernandoGutemberg <fernando70sil@gmail.com>
2024-02-10 13:52:23 -0300

    arrays

--- criandofuncoes/script.js
+++ criandofuncoes/script.js
@@ -1,32 +1,35 @@
 //funcao entrar
 
-function entrar(){
+function entrar() {
 
     var area = document.getElementById('area');
     var texto = prompt('Digite seu nome?');
-    
-    if(texto == '' || texto == null){
+
+    if (texto == '' || texto == null) {
         alert('Digite seu nome novamente!');
         area.innerHTML = 'Bem vindo...';
-    }else{
+    } else {
         area.innerHTML = 'Bem vindo ' + texto;
     }
 
 }
 
-function entrar2(nome){
+function entrar2(nome) {
 
     var area = document.getElementById('area2');
     var texto = prompt('Digite seu sobrenome?');
     area.innerHTML = nome + " " + texto;
 
-
 }
 
-function entrar3(idade){
-
+function entrar3(nome, sobrenome) {
     var area = document.getElementById('area3');
     var texto = prompt('Qual sua idade?');
-    area.innerHTML = nome + " " + sobrenome + " " + idade;
 
+    if (texto == ' ' || texto == null) {
+        alert('Digite sua idade novamente!');
+        area.innerHTML = 'Bem vindo...';
+    } else {
+        area.innerHTML = nome + "  " + sobrenome + " Idade: " + texto;
+    }
 }
